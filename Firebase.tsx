ebfc2d5781aaf6fd5c6ae9d4d0d48179b1bcef68
BachaBech <firasbech@gmail.com>
2023-12-26 18:48:11 +0100

    update auth

--- Firebase.tsx
+++ Firebase.tsx
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
-
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqnWUZdmj0wNzkhrxel-DHHxtCldi5ido",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
